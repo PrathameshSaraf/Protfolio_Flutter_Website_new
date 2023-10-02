ab378b16daafaf25f1cb841d772ca66aee327b9d
pgsar <prathameshsaraf2002@gmail.com>
2023-10-02 17:56:50 +0530

    update  V2

---
+++ lib/app/sections/splashView.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomePage(),));
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor:  Color(0xFF000515),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AnimatedImageContainer(width: 100,height: 100,),
+            SizedBox(height: defaultPadding,),
+            AnimatedLoadingText(),
+          ],
+        ),
+      ),
+    );
+  }
+}
